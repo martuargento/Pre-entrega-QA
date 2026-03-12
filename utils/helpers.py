9c942103a48eb0161b566288ce6966edca4a3fe1
Martin Alejandro Nuñez <martinalejandronuniez@gmail.com>
2026-03-12 01:10:01 -0300

    github actions

--- utils/helpers.py
+++ utils/helpers.py
@@ -13,9 +13,14 @@ URL = "https://www.saucedemo.com/"
 def get_driver():
     # 1. Configuramos las opciones para el robot de GitHub Actions
     chrome_options = Options()
-    chrome_options.add_argument("--headless") 
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # LAS 3 LÍNEAS MÁGICAS PARA GITHUB ACTIONS
+    chrome_options.add_argument("--headless")               # Sin ventana
+    chrome_options.add_argument("--no-sandbox")             # Bypass de seguridad de Linux
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Usa memoria compartida (evita crashes)
+    
+    # Opcional pero recomendado para evitar otros errores
+    chrome_options.add_argument("--window-size=1920,1080")
 
     # 2. Creamos el driver usando esas opciones
     service = Service(ChromeDriverManager().install())
